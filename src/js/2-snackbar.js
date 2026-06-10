64a9a898cff0f900e3bd8bb1fa02658137db8318
bebroid <lukabyckov@gmail.com>
2026-06-10 03:34:16 +0300

    timer-and-snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,27 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const state = event.target.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(`✅ Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise.then(value => {
+    iziToast.success({ message: value, position: 'topRight' });
+  });
+  promise.catch(error => {
+    iziToast.error({ message: error, position: 'topRight' });
+  });
+});
